4bcd029bc9604604716c048d98a8449a1f1f97b3
may huang <mayno1024@gmail.com>
2022-11-21 12:07:15 +0800

    feat: Set express-handlebars

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const express = require('express')
+const app = express()
 const mongoose = require('./config/mongoose')
+const exphbs = require('express-handlebars')
 
+// listen on server
+const port = 3000
+app.listen(port, () => {
+  console.log(`listen on port: ${port}`)
+})
+
+// set templete engine 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+app.get('/',(req, res) => {
+  res.render('index')
+})
