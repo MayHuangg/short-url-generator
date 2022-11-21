5e7c015cbc4badd78d47d253d0c4943e8c3df612
may huang <mayno1024@gmail.com>
2022-11-21 11:13:42 +0800

    feat: Connect to Mongodb

---
+++ config/mongoose.js
@@ -0,0 +1,15 @@
+// connect to mongodb
+require('dotenv').config() 
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+db.on('error',() => {
+  console.log('mongodb error')
+})
+db.once('open',() => {
+  console.log('mongodb connect')
+})
+
+module.exports = db
