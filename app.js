18eda02b7764cdfa0153ea5c0c51c17350e01250
may huang <mayno1024@gmail.com>
2022-11-21 19:58:06 +0800

    feat: Generate random character set

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const mongoose = require('./config/mongoose')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
+// 裝已使用過的5碼組合
+// let usedSet = []
 
 // listen on server
 const port = 3000
@@ -15,4 +19,36 @@ app.set('view engine', 'hbs')
 
 app.get('/',(req, res) => {
   res.render('index')
+})
+
+// 使req.body得以被取得
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// 將網址與隨機五碼存入資料庫
+const Url = require('./models/url')
+app.post('/url',(req, res) => {
+  const url = req.body.url
+  let fiveCharacters = ''
+  
+  // 創出含有大小寫字母和數字的array 
+  function generateCharecters() {
+    const lowercase = [...Array(26)].map((_, i) => String.fromCharCode(i + 97)) 
+    const uppercase = [...Array(26)].map((_, i) => String.fromCharCode(i + 65))
+    const num = [...Array(10)].map((_, i) => i);
+    const allCharecters = lowercase.concat(uppercase).concat(num)
+    return allCharecters
+  }
+  // 從含有大小寫字母和數字的array中挑出五個字組成一組
+  function pickCharacter(allChars) {
+    let result = ''
+    for(let i = 0; i < 5; i++) {
+      result += allChars[Math.floor(Math.random() * allChars.length)]
+    }
+    return result
+  }
+  // 製作出不重複的5碼組合
+  fiveCharacters = pickCharacter(generateCharecters())
+
+  return Url.create({url, fiveCharacters})
+    .then(() => res.redirect('/'))
 })
---
+++ models/url.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const Url = new Schema({
+  url:{
+    type: String,
+    required: true
+  },
+  fiveCharacters:{
+    type: String,
+    required:true
+  }
+})
+
+module.exports = mongoose.model('UrlSchema',Url)
