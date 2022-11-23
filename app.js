0cd74363d1610fcb1b7249cab4f4f737504e786c
may huang <mayno1024@gmail.com>
2022-11-23 23:05:33 +0800

    feat: Pass shorter url

--- app.js
+++ app.js
@@ -30,34 +30,6 @@ app.use(routes)
 
 // ////將網址與隨機五碼存入資料庫
 const Url = require('./models/url')
-// app.post('/url',(req, res) => {
-//   const url = req.body.url
-//   let fiveCharacters = ''
-  
-//   // 創出含有大小寫字母和數字的array 
-//   function generateCharecters() {
-//     const lowercase = [...Array(26)].map((_, i) => String.fromCharCode(i + 97)) 
-//     const uppercase = [...Array(26)].map((_, i) => String.fromCharCode(i + 65))
-//     const num = [...Array(10)].map((_, i) => i);
-//     const allCharecters = lowercase.concat(uppercase).concat(num)
-//     return allCharecters
-//   }
-//   // 從含有大小寫字母和數字的array中挑出五個字組成一組
-//   function pickCharacter(allChars) {
-//     let result = ''
-//     for(let i = 0; i < 5; i++) {
-//       result += allChars[Math.floor(Math.random() * allChars.length)]
-//     }
-//     return result
-//   }
-//   // 製作出不重複的5碼組合
-//   fiveCharacters = pickCharacter(generateCharecters())
-//   // 製作出縮短的網址
-//   const shortUrl = `http://localhost:3000/${fiveCharacters}`
-
-//   return Url.create({url, fiveCharacters})
-//     .then(() => res.render('index',{ shortUrl }))
-// })
 
 ////藉由5碼組合找出對應的資料
 app.get('/:characterSet', (req, res) => {
--- routes/modules/shorter.js
+++ routes/modules/shorter.js
@@ -4,9 +4,27 @@ const Url = require('../../models/url')
 
 ////將網址與隨機五碼存入資料庫
 Router.post('/url',(req, res) => {
-  const url = req.body.url
+  const urlFromClient = req.body.url
   let fiveCharacters = ''
+  let shortUrl = ''
   
+  Url.find( { url:urlFromClient }).lean()
+    .then(data => {
+      // 如果有在資料庫中找到相同url的資料，直接從資料庫中抓出先前生成的短網址 
+      if (data[0]) {
+        shortUrl = `http://localhost:3000/${data[0].fiveCharacters}`
+      } else {
+        // 如果沒有找到相同的url，則製作出短網址並存入資料庫
+        fiveCharacters = pickCharacter(generateCharecters())
+        shortUrl = `http://localhost:3000/${fiveCharacters}`
+        // 之後要想想怎麼確認5碼組合使用過了沒 
+        Url.create({url:urlFromClient, fiveCharacters})
+      } 
+    })
+    .then(() => res.render('index',{ shortUrl }))
+    .catch(err => console.log(err))
+
+  ///若未申請過則需生成新的一組5碼組合
   // 創出含有大小寫字母和數字的array 
   function generateCharecters() {
     const lowercase = [...Array(26)].map((_, i) => String.fromCharCode(i + 97)) 
@@ -23,13 +41,6 @@ Router.post('/url',(req, res) => {
     }
     return result
   }
-  // 製作出不重複的5碼組合
-  fiveCharacters = pickCharacter(generateCharecters())
-  // 製作出縮短的網址
-  const shortUrl = `http://localhost:3000/${fiveCharacters}`
-
-  return Url.create({url, fiveCharacters})
-    .then(() => res.render('index',{ shortUrl }))
 })
 
 module.exports = Router
